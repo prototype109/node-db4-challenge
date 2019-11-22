96de2d2a09d12564ef3329f26fb1bcbf22e370d5
Brandon Nichols <brandonaronnichols@gamil.com>
2019-11-21 20:55:01 -0600

    added tables with constraints to migrations

--- data/migrations/20191121010420_recipeBook-schema.js
+++ data/migrations/20191121010420_recipeBook-schema.js
@@ -13,7 +13,46 @@ exports.up = function(knex) {
         .text("ingredientName")
         .notNullable()
         .unique();
+    })
+    .createTable("recipes_ingredients", tbl => {
+      tbl
+        .integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("recipes")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      tbl
+        .integer("ingredient_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("ingredients")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      tbl.float("quantity", 5, 2).notNullable();
+      tbl.primary(["recipe_id", "ingredient_id"]);
+    })
+    .createTable("steps", tbl => {
+      tbl
+        .integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("recipes")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      tbl.integer("step").notNullable();
+      tbl.string("instruction").notNullable();
+      tbl.primary(["recipe_id", "step"]);
     });
 };
 
-exports.down = function(knex) {};
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists("steps")
+    .dropTableIfExists("recipes_ingredients")
+    .dropTableIfExists("ingredients")
+    .dropTableIfExists("recipes");
+};
